892e9ca0d00419dc87711b91f4ff9caf1289384f
vilodye1 <vilodye@gmail.com>
2023-05-25 11:58:05 -0400

    create listing card component

---
+++ resources/views/components/listing-card.blade.php
@@ -0,0 +1,16 @@
+<div class="container flex flex-col justify-center items-center text-center border mx-auto my-2 p-4 md:max-w-2xl">
+    <div>
+        <img src="../images/no-image.png" alt="" class="w-26 h-24">
+    </div>
+    <div class="flex flex-col">
+        <a href="#" class="text-2xl font-bold mt-2">Laravel Developer</a>
+        <p class="font-bold tracking-wide text-darkGrayishBlue">Lara Corps</p>
+        <!-- Tags -->
+        <div class="p-2 space-x-2">
+            <a href="#" class="bg-royalBlue text-white rounded-full px-2 py-0.5 hover:text-powderblue">UI</a>
+            <a href="#" class="bg-royalBlue text-white rounded-full px-2 py-0.5 hover:text-powderblue">Full Stack</a>
+            <a href="#" class="bg-royalBlue text-white rounded-full px-2 py-0.5 hover:text-powderblue">Vue js</a>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
+    </div>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,74 +25,11 @@
                 <button class="hidden bg-veryLightBlue py-4 px-6 rounded-full absolute right-2 mt-36 lg:block shadow-md hover:bg-hover active:shadow-none transition-all duration-300">></button>
                 <!-- right button -->
                 <button class="hidden bg-veryLightBlue py-4 px-6 rounded-full absolute left-2 mt-36 shadow-md hover:bg-hover active:shadow-none transition-all duration-300"><</button>
-        <!-- Listing 1 -->
-        <div class="container flex flex-col justify-center items-center text-center border mx-auto my-2 p-4 md:max-w-2xl">
-            <div>
-                <img src="../images/no-image.png" alt="" class="w-26 h-24">
-            </div>
-            <div class="flex flex-col">
-                <a href="#" class="text-2xl font-bold mt-2">Laravel Developer</a>
-                <p class="font-bold tracking-wide text-darkGrayishBlue">Lara Corps</p>
-                <!-- Tags -->
-                <div class="p-2 space-x-2">
-                    <a href="#" class="bg-royalBlue text-white rounded-full px-2 py-0.5 hover:text-powderblue">UI</a>
-                    <a href="#" class="bg-royalBlue text-white rounded-full px-2 py-0.5 hover:text-powderblue">Full Stack</a>
-                    <a href="#" class="bg-royalBlue text-white rounded-full px-2 py-0.5 hover:text-powderblue">Vue js</a>
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
-            </div>
-        </div>
-        <!-- Listing 1 -->
-        <div class="container flex flex-col justify-center items-center text-center border mx-auto p-4 my-2 md:max-w-2xl">
-            <div>
-                <img src="../images/no-image.png" alt="" class="w-26 h-24">
-            </div>
-            <div class="flex flex-col">
-                <a href="#" class="text-2xl font-bold mt-2">Laravel Developer</a>
-                <p class="font-bold tracking-wide text-darkGrayishBlue">Lara Corps</p>
-                <!-- Tags -->
-                <div class="p-2 space-x-2">
-                    <a href="#" class="bg-royalBlue text-white rounded-full px-2 py-0.5 hover:text-powderblue">UI</a>
-                    <a href="#" class="bg-royalBlue text-white rounded-full px-2 py-0.5 hover:text-powderblue">Full Stack</a>
-                    <a href="#" class="bg-royalBlue text-white rounded-full px-2 py-0.5 hover:text-powderblue">Vue js</a>
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
-            </div>
-        </div>
-        <!-- Listing 1 -->
-        <div class="container flex flex-col justify-center items-center text-center border mx-auto p-4 my-2 md:max-w-2xl">
-            <div>
-                <img src="../images/no-image.png" alt="" class="w-26 h-24">
-            </div>
-            <div class="flex flex-col">
-                <a href="#" class="text-2xl font-bold mt-2">Laravel Developer</a>
-                <p class="font-bold tracking-wide text-darkGrayishBlue">Lara Corps</p>
-                <!-- Tags -->
-                <div class="p-2 space-x-2">
-                    <a href="#" class="bg-royalBlue text-white rounded-full px-2 py-0.5 hover:text-powderblue">UI</a>
-                    <a href="#" class="bg-royalBlue text-white rounded-full px-2 py-0.5 hover:text-powderblue">Full Stack</a>
-                    <a href="#" class="bg-royalBlue text-white rounded-full px-2 py-0.5 hover:text-powderblue">Vue js</a>
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
-            </div>
-        </div>
-        <!-- Listing 1 -->
-        <div class="container flex flex-col justify-center items-center text-center border mx-auto p-4 my-2 md:max-w-2xl">
-            <div>
-                <img src="../images/no-image.png" alt="" class="w-26 h-24">
-            </div>
-            <div class="flex flex-col">
-                <a href="#" class="text-2xl font-bold mt-2">Laravel Developer</a>
-                <p class="font-bold tracking-wide text-darkGrayishBlue">Lara Corps</p>
-                <!-- Tags -->
-                <div class="p-2 space-x-2">
-                    <a href="#" class="bg-royalBlue text-white rounded-full px-2 py-0.5 hover:text-powderblue">UI</a>
-                    <a href="#" class="bg-royalBlue text-white rounded-full px-2 py-0.5 hover:text-powderblue">Full Stack</a>
-                    <a href="#" class="bg-royalBlue text-white rounded-full px-2 py-0.5 hover:text-powderblue">Vue js</a>
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
-            </div>
-        </div>
+        <!-- Listings Group -->
+        <x-listing-card />
+        <x-listing-card />
+        <x-listing-card />
+        <x-listing-card />
         </div>
         <a href="#" class="float-right mr-10 text-lg hover:text-hover">View all ></a>
     </section>
